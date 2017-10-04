376c5011c06ea1d4ad0307416fab76265637e20f
Tommy Hansen <th@inmobile.dk>
2017-10-04 11:01:12 +0200

    Fixed .gitignore placement

--- Sms.ApiClient/Properties/AssemblyInfo.cs
+++ Sms.ApiClient/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Sms.ApiClient.V2;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -32,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0.1")]
-[assembly: AssemblyFileVersion("2.2.0.1")]
+[assembly: AssemblyVersion(ClientUtils.VersionNumber)]
+[assembly: AssemblyFileVersion(ClientUtils.VersionNumber)]
--- Sms.ApiClient/V2/VersionNumber.cs
+++ Sms.ApiClient/V2/VersionNumber.cs
@@ -2,6 +2,13 @@
 {
 	public static class ClientUtils
 	{
+		/// <summary>
+		/// Version information for an assembly consists of the following four values:
+		/// Major Version
+		/// Minor Version 
+		/// Build Number
+		/// Revision
+		/// </summary>
 		public const string VersionNumber = "2.2.0.1";
 	}
 }
