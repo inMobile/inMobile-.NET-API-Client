538282abebbdcb723c0cf7d86cf38b68be699fd1
Stephan Møller <sr@inmobile.dk>
2021-11-18 10:47:40 +0100

    Attempted to avoid race condition in port allocation in integration tests

--- InMobile.Sms.ApiClient.Test/Tools/LocalPortUtils.cs
+++ InMobile.Sms.ApiClient.Test/Tools/LocalPortUtils.cs
@@ -7,24 +7,28 @@ namespace InMobile.Sms.ApiClient.Test.Tools
     public static class LocalPortUtils
     {
         private static int _lastUsedPort = 2021;
+        private static object _syncLock = new object();
         public static int GetAndReserverAvailablePort()
         {
-            // Gather info about existing ports
-            var portList = new List<int>();
-            var properties = IPGlobalProperties.GetIPGlobalProperties();
-            portList.AddRange(properties.GetActiveTcpConnections().Select(c => c.LocalEndPoint.Port));
-            portList.AddRange(properties.GetActiveTcpListeners().Select(c => c.Port));
-            portList.AddRange(properties.GetActiveUdpListeners().Select(c => c.Port));
-
-            _lastUsedPort++;
-            while (portList.Contains(_lastUsedPort))
+            lock (_syncLock)
             {
-                _lastUsedPort++;
-            }
+                // Gather info about existing ports
+                var portList = new List<int>();
+                var properties = IPGlobalProperties.GetIPGlobalProperties();
+                portList.AddRange(properties.GetActiveTcpConnections().Select(c => c.LocalEndPoint.Port));
+                portList.AddRange(properties.GetActiveTcpListeners().Select(c => c.Port));
+                portList.AddRange(properties.GetActiveUdpListeners().Select(c => c.Port));
 
-            var portToUse = _lastUsedPort;
-            _lastUsedPort++;
-            return portToUse;
+                _lastUsedPort++;
+                while (portList.Contains(_lastUsedPort))
+                {
+                    _lastUsedPort++;
+                }
+
+                var portToUse = _lastUsedPort;
+                _lastUsedPort++;
+                return portToUse;
+            }
         }
 
     }
