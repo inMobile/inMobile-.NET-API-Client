f9550b91f7d2abc15b76dd6d6a39d165c906d25f
Kris Thomsen <mail@kristhomsen.dk>
2023-09-05 10:09:26 +0200

    Implemented GDPR DeletionRequest support

---
+++ Inmobile.Sms.ApiClient/SmsGdpr/SmsGdprDeletionRequestId.cs
@@ -0,0 +1,16 @@
+﻿namespace InMobile.Sms.ApiClient
+{
+    /// <summary>
+    /// The id type used for GDPR deletion request.
+    /// </summary>
+    public class SmsGdprDeletionRequestId : CustomStringTypeBase<SmsGdprDeletionRequestId>
+    {
+        /// <summary>
+        /// Create a new GDPR deletion request id.
+        /// </summary>
+        /// <param name="value"></param>
+        public SmsGdprDeletionRequestId(string value) : base(value)
+        {
+        }
+    }
+}
