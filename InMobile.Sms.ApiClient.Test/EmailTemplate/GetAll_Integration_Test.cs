5375418be0867dca192d4a17966fb8b95a4d911f
kristho91 <mail@kristhomsen.dk>
2024-01-23 16:31:58 +0100

    Initial implementation of EmailTemplateApiMethods

---
+++ InMobile.Sms.ApiClient.Test/EmailTemplate/GetAll_Integration_Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace InMobile.Sms.ApiClient.Test.EmailTemplate
+{
+    public class GetAll_Integration_Test
+    {
+        [Fact]
+        public void EmptyResult_Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void SinglePage_Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void MultiPage_Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ApiError_Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InMobile.Sms.ApiClient.Test/EmailTemplate/GetById_Integration_Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+
+namespace InMobile.Sms.ApiClient.Test.EmailTemplate
+{
+    public class GetById_Integration_Test
+    {
+        [Fact]
+        public void GetById_Success_Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetById_ApiError_NotFound_Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetById_ApiError_InternalServerError_Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
