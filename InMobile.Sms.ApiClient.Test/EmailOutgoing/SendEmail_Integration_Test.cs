6da096c15c5a0ccc4c33243eccefe99c60d6f786
Kris Thomsen <kristho91@users.noreply.github.com>
2025-01-15 14:48:59 +0100

    Kt/add email reply to recipient (#17)
    * Added new EmailReplyToRecipient type
    
    * Fix tests
    
    * Added NoReply example to ApiTestRunner
    
    * Add ApiTestRunner non-async too

--- InMobile.Sms.ApiClient.Test/EmailOutgoing/SendEmail_Integration_Test.cs
+++ InMobile.Sms.ApiClient.Test/EmailOutgoing/SendEmail_Integration_Test.cs
@@ -81,7 +81,7 @@ public class SendEmail_Integration_Test
     [Fact]
     public void SendEmail_WithAllFields_Success_Test()
     {
-        var expectedRequestJson = @"{""From"":{""EmailAddress"":""noreply@inmobile.com"",""DisplayName"":""X from inMobile""},""To"":[{""EmailAddress"":""test@inmobile.com"",""DisplayName"":""Test""}],""ReplyTo"":[{""EmailAddress"":""support@inmobile.com"",""DisplayName"":""Y from inMobile""}],""MessageId"":""558c8a34-531d-4ad4-8580-22b554ed00c1"",""SendTime"":""2020-10-10T16:13:00Z"",""Tracking"":false,""Subject"":""This is a subject!"",""Html"":""<!DOCTYPE html><html><head></head><body><p>This is my HTML</p></body></html>"",""Text"":""Text edition""}";
+        var expectedRequestJson = @"{""From"":{""EmailAddress"":""noreply@inmobile.com"",""DisplayName"":""X from inMobile""},""To"":[{""EmailAddress"":""test@inmobile.com"",""DisplayName"":""Test""}],""ReplyTo"":[{""EmailAddress"":""support@inmobile.com""}],""MessageId"":""558c8a34-531d-4ad4-8580-22b554ed00c1"",""SendTime"":""2020-10-10T16:13:00Z"",""Tracking"":false,""Subject"":""This is a subject!"",""Html"":""<!DOCTYPE html><html><head></head><body><p>This is my HTML</p></body></html>"",""Text"":""Text edition""}";
         var responseJson = @"{
     ""messageId"": ""558c8a34-531d-4ad4-8580-22b554ed00c1"",
     ""to"": [
@@ -103,7 +103,7 @@ public class SendEmail_Integration_Test
                 html: "<!DOCTYPE html><html><head></head><body><p>This is my HTML</p></body></html>",
                 from: new EmailSender(emailAddress: "noreply@inmobile.com", displayName: "X from inMobile"),
                 to: new List<EmailRecipient> { new EmailRecipient(emailAddress: "test@inmobile.com", displayName: "Test") },
-                replyTo: new List<EmailRecipient> { new EmailRecipient(emailAddress: "support@inmobile.com", displayName: "Y from inMobile") },
+                replyTo: new List<EmailReplyToRecipient> { new EmailReplyToRecipient(emailAddress: "support@inmobile.com") },
                 text: "Text edition",
                 sendTime: new DateTime(2020, 10, 10, 16, 13, 0, kind: DateTimeKind.Utc),
                 tracking: false,
@@ -120,7 +120,7 @@ public class SendEmail_Integration_Test
     [Fact]
     public async Task SendEmailAsync_WithAllFields_Success_Test()
     {
-        var expectedRequestJson = @"{""From"":{""EmailAddress"":""noreply@inmobile.com"",""DisplayName"":""X from inMobile""},""To"":[{""EmailAddress"":""test@inmobile.com"",""DisplayName"":""Test""}],""ReplyTo"":[{""EmailAddress"":""support@inmobile.com"",""DisplayName"":""Y from inMobile""}],""MessageId"":""558c8a34-531d-4ad4-8580-22b554ed00c1"",""SendTime"":""2020-10-10T16:13:00Z"",""Tracking"":false,""Subject"":""This is a subject!"",""Html"":""<!DOCTYPE html><html><head></head><body><p>This is my HTML</p></body></html>"",""Text"":""Text edition""}";
+        var expectedRequestJson = @"{""From"":{""EmailAddress"":""noreply@inmobile.com"",""DisplayName"":""X from inMobile""},""To"":[{""EmailAddress"":""test@inmobile.com"",""DisplayName"":""Test""}],""ReplyTo"":[{""EmailAddress"":""support@inmobile.com""}],""MessageId"":""558c8a34-531d-4ad4-8580-22b554ed00c1"",""SendTime"":""2020-10-10T16:13:00Z"",""Tracking"":false,""Subject"":""This is a subject!"",""Html"":""<!DOCTYPE html><html><head></head><body><p>This is my HTML</p></body></html>"",""Text"":""Text edition""}";
         var responseJson = @"{
     ""messageId"": ""558c8a34-531d-4ad4-8580-22b554ed00c1"",
     ""to"": [
@@ -142,7 +142,7 @@ public class SendEmail_Integration_Test
                 html: "<!DOCTYPE html><html><head></head><body><p>This is my HTML</p></body></html>",
                 from: new EmailSender(emailAddress: "noreply@inmobile.com", displayName: "X from inMobile"),
                 to: new List<EmailRecipient> { new EmailRecipient(emailAddress: "test@inmobile.com", displayName: "Test") },
-                replyTo: new List<EmailRecipient> { new EmailRecipient(emailAddress: "support@inmobile.com", displayName: "Y from inMobile") },
+                replyTo: new List<EmailReplyToRecipient> { new EmailReplyToRecipient(emailAddress: "support@inmobile.com") },
                 text: "Text edition",
                 sendTime: new DateTime(2020, 10, 10, 16, 13, 0, kind: DateTimeKind.Utc),
                 tracking: false,
---
+++ InMobile.Sms.ApiClient/EmailOutgoing/SendEmail/EmailReplyToRecipient.cs
@@ -0,0 +1,21 @@
+﻿namespace InMobile.Sms.ApiClient
+{
+    /// <summary>
+    /// Information about the email replyto recipient.
+    /// </summary>
+    public class EmailReplyToRecipient
+    {
+        /// <summary>
+        /// The email address of the recipient.
+        /// </summary>
+        public string EmailAddress { get; }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="emailAddress">The email address of the recipient</param>
+        public EmailReplyToRecipient(string emailAddress)
+        {
+            EmailAddress = emailAddress;
+        }
+    }
+}
